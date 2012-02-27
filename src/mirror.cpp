9ca3955f6de49a36853673ec59261105dd86ba1f
Wenzel Jakob <wenzel@cs.cornell.edu>
2012-02-27 11:27:51 -0500

    got rid of some warnings

--- src/mirror.cpp
+++ src/mirror.cpp
@@ -24,7 +24,7 @@ NORI_NAMESPACE_BEGIN
 /// Ideal mirror BRDF
 class Mirror : public BSDF {
 public:
-	Mirror(const PropertyList &propList) { }
+	Mirror(const PropertyList &) { }
 
 	Color3f eval(const BSDFQueryRecord &) const {
 		/* Discrete BRDFs always evaluate to zero in Nori */
@@ -36,7 +36,7 @@ public:
 		return 0.0f;
 	}
 
-	Color3f sample(BSDFQueryRecord &bRec, const Point2f &sample) const {
+	Color3f sample(BSDFQueryRecord &bRec, const Point2f &) const {
 		if (Frame::cosTheta(bRec.wi) <= 0) 
 			return Color3f(0.0f);
 
